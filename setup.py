495ca5f972b537b5a3bf6c82aa5bb74b02a31f90
Devendra Goswami <pdevendragoswami@gmail>
2023-12-06 21:40:39 +0530

    setup files updated

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+__version__ = '0.0.1'
+
+Repo_Name = 'CNNClassifier'
+Author_Name = 'Devendra Goswami'
+Src_Repo = 'CNNClassifier'
+Author_Email = 'pdevendragoswami@gmail.com'
+
+with open('README.md','r') as file:
+    long_description = file.read()
+
+setuptools.setup(
+    name = Src_Repo,
+    version = __version__,
+    author = Author_Name,
+    author_email = Author_Email,
+    description = 'End to end Image Classification',
+    long_description = long_description,
+    long_description_content = 'text/markdown',
+    url = f"https://github.com/{Author_Name}/{Repo_Name}",
+    project_url = {"Bug Tracker":f"https://github.com/{Author_Name}/{Repo_Name}"},
+    package_dir = {"":"src"},
+    packages=setuptools.find_packages(where='src')
+
+)
