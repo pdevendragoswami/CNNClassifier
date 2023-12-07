97c041756d499e028e4901036d71ed1695a575eb
Devendra Goswami <pdevendragoswami@gmail.com>
2023-12-07 13:00:48 +0530

    configuration file updated for training

--- src/CNNClassifier/config/configuration.py
+++ src/CNNClassifier/config/configuration.py
@@ -1,7 +1,7 @@
 import os
 from pathlib import Path
 from CNNClassifier.utils.utils import read_yaml,create_directory
-from CNNClassifier.entity.config_entity import DataIngestionConfig,PrepareBaseModelConfig
+from CNNClassifier.entity.config_entity import DataIngestionConfig,PrepareBaseModelConfig,TrainingConfig
 from CNNClassifier.constants import CONFIG_FILE_PATH,PARAMS_FILE_PATH
 
 class ConfigurationManager:
@@ -37,4 +37,23 @@ class ConfigurationManager:
             params_classes = self.params.CLASSES)
 
         return prepare_base_model_config
+
+    def get_training_config(self)-> TrainingConfig :
+        config = self.config.training
+        prepare_base_model = self.config.prepare_base_model
+        params = self.params
+        training_data = os.path.join(self.config.data_ingestion.unzip_dir,'PetImages')
+        create_directory([config.root_dir])
+
+        training_config = TrainingConfig(
+            root_dir = Path(config.root_dir),
+            trained_model_path = Path(config.trained_model_path),
+            updated_base_model_path = Path(prepare_base_model.updated_base_model_path),
+            training_data = Path(training_data),
+            params_epochs = params.EPOCHS,
+            params_batch_size = params.BATCH_SIZE,
+            params_is_augmentation = params.AUGMENTATION,
+            params_image_size = params.IMAGE_SIZE)
+
+        return training_config
  
