aaabe014ca02ad407540befe615cbbcfa8777817
Devendra Goswami <pdevendragoswami@gmail.com>
2023-12-07 13:06:51 +0530

    training pipeline done

--- src/CNNClassifier/pipeline/training.py
+++ src/CNNClassifier/pipeline/training.py
@@ -2,6 +2,7 @@ from CNNClassifier.components.stage_01_data_ingestion import DataIngestion
 from CNNClassifier.config.configuration import ConfigurationManager
 from CNNClassifier import logger
 from CNNClassifier.components.stage_02_prepare_base_model import PrepareBaseModel
+from CNNClassifier.components.stage_03_train import Training
 
 
 config = ConfigurationManager()
@@ -19,3 +20,15 @@ prepare_base_model_config = config.get_prepare_base_model_config()
 prepare_base_model = PrepareBaseModel(config=prepare_base_model_config)
 prepare_base_model.get_base_model()
 prepare_base_model.update_base_model()
+
+
+
+
+
+#config = ConfigurationManager()
+
+training_config = config.get_training_config()
+training = Training(config=training_config)
+training.get_base_model()
+training.train_valid_generator()
+training.train()
