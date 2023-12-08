c4cbd2ae1727aeebf5d83c6d2a5278d34e759eab
Devendra Goswami <pdevendragoswami@gmail.com>
2023-12-08 18:27:46 +0530

    training pipeline updated

--- src/CNNClassifier/pipeline/training.py
+++ src/CNNClassifier/pipeline/training.py
@@ -2,7 +2,8 @@ from CNNClassifier.components.stage_01_data_ingestion import DataIngestion
 from CNNClassifier.config.configuration import ConfigurationManager
 from CNNClassifier import logger
 from CNNClassifier.components.stage_02_prepare_base_model import PrepareBaseModel
-from CNNClassifier.components.stage_03_train import Training
+from CNNClassifier.components.stage_04_train import Training
+from CNNClassifier.components.stage_03_prepare_callback import PrepareCallback
 
 
 config = ConfigurationManager()
@@ -22,13 +23,13 @@ prepare_base_model.get_base_model()
 prepare_base_model.update_base_model()
 
 
-
-
-
 #config = ConfigurationManager()
-
+prepare_callbacks_config = config.get_prepare_callback_config()
+prepare_callbacks = PrepareCallback(config=prepare_callbacks_config)
+callback_list = prepare_callbacks.get_tb_ckpt_callbacks()
 training_config = config.get_training_config()
 training = Training(config=training_config)
 training.get_base_model()
 training.train_valid_generator()
+training.train(callback_list=callback_list)
 training.train()
